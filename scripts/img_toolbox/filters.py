4db1350921661c76f8fc9aea23ea728c056f0546
Jep Etau <jep.etau@gmail.com>
2023-07-05 10:52:30 +0200

    cleaned up database and prepared for audio

--- scripts/img_toolbox/filters.py
+++ scripts/img_toolbox/filters.py
@@ -585,14 +585,14 @@ def  filter_morphologyEx(image, type, radius, iterations):
 
 
 def auto_canny(image, sigma=0.33):
-	# compute the median of the single channel pixel intensities
-	v = np.median(image)
-	# apply automatic Canny edge detection using the computed median
-	lower = int(max(0, (1.0 - sigma) * v))
-	upper = int(min(255, (1.0 + sigma) * v))
-	edged = cv2.Canny(image, lower, upper)
-	# return the edged image
-	return edged
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+    # return the edged image
+    return edged
 
 def strokeEdges(src, dst, blurKsize = 7, edgeKsize = 5):
    if blurKsize >= 3:
